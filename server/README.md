653a7a295f612b6df7a8b8fe1d48e1508399b18c
Ishmael Gyaban <Ishmael.Gyaban@wemabank.com>
2018-11-19 20:16:37 +0100

    completed the react frontend

--- server/README.md
+++ server/README.md
@@ -5,3 +5,5 @@ Steps to run this project:
 1. Run `npm i` command
 2. Setup database settings inside `ormconfig.json` file
 3. Run `npm start` command
+
+NB: Added .env file because the secret are just for testing. You can replace with your own.
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { createGlobalStyle } from "styled-components";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from './serviceWorker';
+import { Routes } from "./Routes";
+
+const client = new ApolloClient({
+    uri: "http://localhost:4000/graphql",
+    credentials: "include"
+  });
+  
+  const GlobalStyle = createGlobalStyle`
+    body {
+      background-color: rgb(255, 254, 252);
+    }
+    *:focus {
+      outline: 0;
+    }
+    a {
+      color: #0d0d0d;
+      text-decoration: none;
+    }
+  `;
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+    <GlobalStyle />
+    <Routes />
+  </ApolloProvider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
